2907391ab753b5d0533ed65aa6204badc6e508d2
crhino <piraino.chris@gmail.com>
2015-09-19 22:38:32 -0700

    Finish writeLogs function
    Takes in a Producer of UDP Messages and writes them to a file

--- HSpecTests.hs
+++ HSpecTests.hs
@@ -1,10 +1,37 @@
+{-# LANGUAGE Rank2Types #-}
+
 module Main where
 
 import Reservoir
 import Test.Hspec
+import Data.Conduit
+import Data.Conduit.Network.UDP
+import Data.Conduit.List
+import Data.ByteString
+import Data.Word
+import Network.Socket.Internal
+import Control.Monad.Trans.Resource
+
+-- For removing the test file
+import System.Directory
+import Control.Exception
+import System.IO.Error
+
+testReservoir :: IO ()
+testReservoir
+  = writeLogs (source :: Producer (ResourceT IO) Message) "test.out"
+  where source = sourceList [Message bytes addr]
+        bytes = (singleton (51 :: Word8))
+        addr = SockAddrUnix "a unix addr"
 
 main :: IO ()
-main = hspec $ do
-  describe "Validate hello function" $ do
-    it "hello is supposed to prefix Hello! to things" $ do
-      hello "me" `shouldBe` ("Hello, me" :: String)
+main = hspec $ after_ (removeIfExists "test.out") $ do
+  describe "Reservoir" $ do
+    it "listens on a UDP socket" $ do
+      testReservoir `shouldReturn` ()
+
+removeIfExists :: FilePath -> IO ()
+removeIfExists fileName = removeFile fileName `catch` handleExists
+  where handleExists e
+          | isDoesNotExistError e = return ()
+          | otherwise = throwIO e
--- Reservoir.hs
+++ Reservoir.hs
@@ -1,8 +1,19 @@
+{-# LANGUAGE Rank2Types #-}
+
 module Reservoir where
 
-import System.Environment
+-- import System.Environment
+import Data.ByteString
+import Data.Conduit
+import Data.Conduit.Binary
+import Data.Conduit.Network.UDP
+import Control.Monad.Trans.Resource
 
-reservoir :: IO ()
-reservoir = getArgs >>= print . hello . head
+-- reservoir :: IO ()
+-- reservoir = getArgs >>= writeLogs
 
-hello s = "Hello, " ++ s
+writeLogs :: Producer (ResourceT IO) Message -> String -> IO ()
+writeLogs s o = runResourceT $ (mapOutput transformPkts s) $$ sinkFile o
+
+transformPkts :: Message -> ByteString
+transformPkts (Message b _) = b
